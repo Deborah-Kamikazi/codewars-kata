a85012c62548aaa605ce36992ecf5213d01a29b5
Kamikazi-Deborah <deborahkamikazi9@gmail.com>
2024-04-18 10:36:44 +0200

    the function that returns

---
+++ returns.js
@@ -0,0 +1,11 @@
+const add  = (a,b) =>{a+b}
+
+const subtract = (a,b) =>{a-b}
+
+const multiply = (a,b) =>{a*b}
+
+const divide = (a,b) =>{a/b}
+
+const modulo = (a,b) =>{a%b}
+
+const exponentiate = (a,b) =>{a**b}
